5d60efc7797d4552f05cc557a01b7ed50d6f3fd0
Axel Forsman <axelsfor@gmail.com>
2022-11-30 20:14:18 +0100

    Specify Nix as dependency of texlive-nix-pm

--- packages/texlive-nix-pm/default.nix
+++ packages/texlive-nix-pm/default.nix
@@ -1,7 +1,7 @@
-{ lib, runCommandLocal, makeWrapper, bash, curl, lzma }:
+{ lib, runCommandLocal, makeWrapper, bash, curl, lzma, nix }:
 runCommandLocal "texlive-nix-pm"
   { nativeBuildInputs = [ makeWrapper ]; }
   ''
   makeWrapper ${./texlive-nix-pm} $out/bin/texlive-nix-pm \
-    --prefix PATH : ${lib.makeBinPath [ bash curl lzma ]}
+    --prefix PATH : ${lib.makeBinPath [ bash curl lzma nix ]}
   ''
