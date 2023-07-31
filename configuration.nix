4e1b8b5a985fdb5e816d6734573db32c19164970
Axel Forsman <axelsfor@gmail.com>
2023-07-31 16:59:40 +0200

    Use the Nix Community binary cache

--- configuration.nix
+++ configuration.nix
@@ -91,11 +91,12 @@
   nix.settings = {
     experimental-features = [ "nix-command" "flakes" ];
 
-    # Binary cache for Haskell.nix
     substituters = [
-      "https://cache.iog.io"
+      "https://nix-community.cachix.org"
+      "https://cache.iog.io" # Binary cache for Haskell.nix
     ];
     trusted-public-keys = [
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ="
     ];
   };
