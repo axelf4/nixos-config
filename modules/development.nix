40182b0d70201dc19b0fee31d5291d8c7a1f8b0a
Axel Forsman <axel@axelf.se>
2024-05-01 10:38:30 +0200

    Lower kernel.perf_event_paranoid for rr

--- modules/development.nix
+++ modules/development.nix
@@ -22,6 +22,9 @@ in {
     environment.variables.DICPATH = lib.makeSearchPath "share/hunspell"
       (with pkgs.hunspellDicts; [ en-us sv-se ]);
 
+    # Allow kernel profiling for all users (needed by rr)
+    boot.kernel.sysctl."kernel.perf_event_paranoid" = 1;
+
     virtualisation.podman = {
       enable = true;
       dockerCompat = true;
