4db69b1b6131ea1a0b012ba08b33b910742b5a23
Axel Forsman <axel@axelf.se>
2025-07-23 21:53:03 +0200

    Keep build-time dependencies

--- modules/development.nix
+++ modules/development.nix
@@ -28,6 +28,12 @@ in {
     # Allow kernel profiling for all users (needed by rr)
     boot.kernel.sysctl."kernel.perf_event_paranoid" = 1;
 
+    # Keep build-time dependencies.
+    #
+    # Marks outputs of derivations (including those from which rooted
+    # store paths were built, due to keep-derivations).
+    nix.settings.keep-outputs = true;
+
     virtualisation.podman = {
       enable = true;
       dockerCompat = true;
